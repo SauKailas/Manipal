03893611a9984a4d33d2b2f5ac04da016d4e1768
Saurabh Kailas <90869750+SauKailas@users.noreply.github.com>
2024-02-11 18:47:20 +0530

    Delete VI Semester/DSL/Lab5 directory

--- VI Semester/DSL/Lab5/CovidMapper.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('Datasets/covid_19_data.csv')
-
-for date in df["ObservationDate"]:
-    print('%s,,%s' % (date, 1))
--- VI Semester/DSL/Lab5/HeartMapper.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('Datasets/heart_disease_data.csv')
-
-for age in df["age"]:
-    print('%s,,%s' % (age, 1))
